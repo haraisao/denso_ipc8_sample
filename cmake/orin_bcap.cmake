2c5bed2f35cda0b4d75a4f01227b3120e355030d
Isao Hara <hara@rt-net.jp>
2023-04-17 10:20:46 +0900

    Update CMakeLists.txt

---
+++ cmake/orin_bcap.cmake
@@ -0,0 +1,21 @@
+#
+#
+#
+#   ORiN Server 
+find_file(ORIN_LIB NAME libbcap_server.a PATHS "/usr/local/ros/*")
+if (ORIN_LIB)
+  set(ORIN_SERVER_LIBRARIES ${ORIN_LIB} pthread)
+  message(STATUS "orin_bcap header: ${ORIN_LIB}")
+else()
+  message(FATAL_ERROR "header file of orin_bcap server not found")
+endif()
+
+find_path(ORIN_INC bCAPServer/bcap_server.h PATHS "/usr/local/ros/include/*")
+if (ORIN_INC)
+  set(ORIN_INCLUDE_PATH ${ORIN_INC})
+  message(STATUS "orin_bcap library: ${ORIN_INC}")
+else()
+  message(FATAL_ERROR "orin_bcap server library not found")
+endif()
+###############
+
---
+++ scripts/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export RC8SERVER_DIR=/usr/local/rc8server/
+${RC8SERVER_DIR}/bin/rc8server
